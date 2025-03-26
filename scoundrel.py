d5cf032b1c1054b636efbc786f6e2732d62f8c5c
willieeaton <philip.w.eaton@gmail.com>
2025-03-25 21:24:19 -0400

    Finished basic pseudocode structure pass

--- scoundrel.py
+++ scoundrel.py
@@ -1,6 +1,25 @@
 # scoundrel.py - attempt to implement the card-game roguelike Scoundrel in Python
 import random
 
+# introduce global variables and initialize variables for new game
+dungeon = []
+for i in range(1, 53):
+   dungeon.append(i) #ace-through-king of hearts, diamonds, spades, clubs
+for i in [1,11,12,13,14,24,25,26]:
+    dungeon.remove(i) # remove the ace and face cards from hearts and diamonds
+random.shuffle(dungeon)
+health = 20
+weapon = 0
+weapon_degradation = 0
+has_fled = False
+has_drank_potion = False
+room = []
+for i in range(0, 4):
+    room.append(dungeon[0]) # get the four starter cards and remove from dungeon
+    dungeon.pop(0)
+
+# TODO help/credits/play menu
+
 def card_name(card):
     name = "The "
     if card % 13 > 1 and card % 13 < 11:
@@ -24,28 +43,26 @@ def card_name(card):
         name += "Clubs - a monster"
     return name
 
-#initialize variables for new game
-dungeon = []
-for i in range(1, 53):
-   dungeon.append(i) #ace-through-king of hearts, diamonds, spades, clubs
-for i in [1,11,12,13,14,24,25,26]:
-    dungeon.remove(i) # remove the ace and face cards from hearts and diamonds
-random.shuffle(dungeon)
-health = 20
-weapon = 0
-can_flee = True
+def room_description():
+    # give list of items in room
+    print("In this room, are the following objects:")
+    i = 1
+    for j in room:
+        print(str(i) + ") " + card_name(j))
+        i += 1
+    #print current health and weapon
 
-room = []
-for i in range(0, 4):
-    room.append(dungeon[0])
-    dungeon.pop(0)
+def interact_object():
+    # if heart, health potion:
+        # if not used potion in room, gain health up to 20
+        # if has used health or at full, confirm discard
+    # if diamond, gain weapon
+    # if club/spade, fight
+        # if weapon and not degraded, offer weapon choice
+        # game over if health hits under 1
 
-print("In this room, are the following objects:")
-i = 1
-for j in room:
-    print(str(i) + ") " + card_name(j))
-    i += 1
-
+room_description() # display room info
+# TODO: give options
 print("Would you like to engage with an object (1-4) or L)eave the room?")
 while True:
     response = input()
@@ -53,7 +70,7 @@ while True:
         response = int(response)
     except:
         if  lower(response) == 'l':
-            #tryleaveroom
+            #TODO try to leave room
             break
         else:
             print("Please enter a number or \"L\".")
@@ -64,7 +81,7 @@ while True:
     else:
         #tryinteractobject
 
-# loop until 1 object left in room
+# if 1 object left in room, 'new room' (pop on up to 3 more cards)
 # loop until health <= 0 or dungeon and room are empty
 # assign player score
 
